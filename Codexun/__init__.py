9f4729e02c2017e83f13864a8b8c95bf63064f8c
amiralizai2006-stack <amiralizai2006@gmail.com>
2026-09-24 06:18:43 +0430

    Update __init__.py

--- Codexun/__init__.py
+++ Codexun/__init__.py
@@ -1,62 +1,105 @@
-import asyncio
-import importlib
-from pytgcalls import PyTgCalls
+import os
 import time
+import importlib
+import asyncio
+
 from pyrogram import Client
+from pytgcalls import PyTgCalls
+
 from Codexun import config
 
 
 SUDO_USERS = config.SUDO_USERS
 OWNER_ID = config.OWNER_ID
 BOT_ID = config.BOT_ID
+
 BOT_NAME = ""
 BOT_USERNAME = ""
+
 ASSID = config.ASSID
 ASSNAME = ""
 ASSUSERNAME = ""
+
 SUDOERS = SUDO_USERS
 OWNER = OWNER_ID
 
-### Boot Time
+# Boot Time
 boottime = time.time()
 
-### auto
-smexy = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+# =========================================================
+# Assistant STRING SESSION
+# =========================================================
+
+STRING_SESSION = os.getenv("STRING_SESSION")
+
+if not STRING_SESSION:
+    raise RuntimeError(
+        "STRING_SESSION is missing. "
+        "Please add STRING_SESSION to Deployzy Environment Variables."
+    )
+
+# Assistant account
+smexy = Client(
+    "assistant",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    session_string=STRING_SESSION,
+)
+
+# PyTgCalls uses the same Assistant client
 pytgcalls = PyTgCalls(smexy)
 
-### Music Start Time
+# Music Start Time
 Music_START_TIME = time.time()
+
+# =========================================================
+# Bot client
+# =========================================================
 
 app = Client(
     "codexunmusic",
-    config.API_ID,
-    config.API_HASH,
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,
 )
 
-client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
-
+# =========================================================
+# Information
+# =========================================================
 
 def all_info(app, client):
     global BOT_ID, BOT_NAME, BOT_USERNAME
     global ASSID, ASSNAME, ASSUSERNAME
+
     getme = app.get_me()
     getme1 = client.get_me()
+
     BOT_ID = getme.id
     ASSID = getme1.id
+
     if getme.last_name:
-        BOT_NAME = getme.first_name + " " + getme.last_name
+        BOT_NAME = f"{getme.first_name} {getme.last_name}"
     else:
         BOT_NAME = getme.first_name
+
     BOT_USERNAME = getme.username
-    ASSNAME = (
-        f"{getme1.first_name} {getme1.last_name}"
-        if getme1.last_name
-        else getme1.first_name
-    )
+
+    if getme1.last_name:
+        ASSNAME = f"{getme1.first_name} {getme1.last_name}"
+    else:
+        ASSNAME = getme1.first_name
+
     ASSUSERNAME = getme1.username
 
 
+# =========================================================
+# Start
+# =========================================================
+
 app.start()
-client.start()
-all_info(app, client)
+smexy.start()
+
+all_info(app, smexy)
+
+# Keep old variable name compatible with the rest of the project
+client = smexy
